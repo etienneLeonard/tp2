ad5664b8df04687f1fb1cda136d8ee3aa1518528
etienneLeonard <1743287@cstj.qc.ca>
2020-11-10 15:35:55 -0500

    bottom navigation

---
+++ app/src/main/java/ca/qc/cstj/tp2/BottomActivity.kt
@@ -0,0 +1,24 @@
+package ca.qc.cstj.tp2
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_bottom.*
+
+class BottomActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.succursalesFragment, R.id.categoriesFragment, R.id.aProposFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+}
