605cae5e87df7a8ba30243812b26db6d4cf327a3
eti246 <desrocherset@hotmail.com>
2020-11-23 16:58:18 -0500

    e

---
+++ app/src/main/java/ca/qc/cstj/tp2/models/Commentaire.kt
@@ -0,0 +1,6 @@
+package ca.qc.cstj.tp2.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Commentaire (val name:String,val dateCommentaire:String,val message:String,val etoile:Float):java.io.Serializable
