6b8e9d7f2021b60b70f3cdc78b6c3e2c43c2d571
gabrieldgodon <43552845+gabrieldgodon@users.noreply.github.com>
2020-11-23 18:10:28 -0500

    A

--- app/src/main/java/ca/qc/cstj/tp2/detailLivreFragment.kt
+++ app/src/main/java/ca/qc/cstj/tp2/detailLivreFragment.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
+import android.widget.ListAdapter
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.qc.cstj.tp2.adapters.CommentaireRecyclerViewAdapter
 import ca.qc.cstj.tp2.helpers.RepositoryResult
 import ca.qc.cstj.tp2.helpers.Services
+import ca.qc.cstj.tp2.helpers.TopSpacingItemDecoration
+import ca.qc.cstj.tp2.models.Commentaire
+import ca.qc.cstj.tp2.repositories.CommentaireRepository
 import ca.qc.cstj.tp2.repositories.LivreRepository
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpPost
@@ -26,6 +33,7 @@ class detailLivreFragment : Fragment() {
 
 
     private val args: detailLivreFragmentArgs by navArgs()
+    private  lateinit var commentaireRecyclerViewAdapter: CommentaireRecyclerViewAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +48,21 @@ class detailLivreFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail_livre, container, false)
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        commentaireRecyclerViewAdapter = CommentaireRecyclerViewAdapter()
+
+        val topSpacingItemDecoration = TopSpacingItemDecoration(35)
+
+        rcvCommentaires.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = commentaireRecyclerViewAdapter
+
+            addItemDecoration(topSpacingItemDecoration)
+        }
 
         lifecycleScope.launch{
             when(val result = LivreRepository.getLivre(args.id)){
@@ -57,6 +77,17 @@ class detailLivreFragment : Fragment() {
 
                     txvAuteur.text = result.data.auteur
                     txvIsbn.text = result.data.ISBN
+
+                    when(val result = CommentaireRepository.getCommentaire())
+                    {
+                        is RepositoryResult.Success -> {
+                            commentaireRecyclerViewAdapter.commentaires = result.data
+                            rcvCommentaires.adapter!!.notifyDataSetChanged()
+                        }
+                        is RepositoryResult.Error -> {
+                            Toast.makeText(this@detailLivreFragment.context, result.exception.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
 
                     if (result.data.Img.isNotEmpty())
                         Picasso.get().load(result.data.Img).into(imgImage)
