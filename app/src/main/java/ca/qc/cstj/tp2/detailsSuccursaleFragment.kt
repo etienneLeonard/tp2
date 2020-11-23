a13f899214f8a6a65ba7882f6cc118b2385c8091
etienneLeonard <1743287@cstj.qc.ca>
2020-11-23 14:19:59 -0500

    Commit EtienneL

--- app/src/main/java/ca/qc/cstj/tp2/detailsSuccursaleFragment.kt
+++ app/src/main/java/ca/qc/cstj/tp2/detailsSuccursaleFragment.kt
@@ -3,6 +3,7 @@ package ca.qc.cstj.tp2
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
@@ -10,7 +11,7 @@ import kotlinx.android.synthetic.main.fragment_details_succursale.*
 
 class detailsSuccursaleFragment : Fragment() {
 
-    //on va récupérer les arguments du fragment
+    // EL : on va récupérer les arguments du fragment
     private val args: detailsSuccursaleFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +23,21 @@ class detailsSuccursaleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_details_succursale, container, false)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            activity?.onBackPressed()
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //changer le titre dans le barre en haut
+        //EL : changer le titre dans le barre en haut
         (activity as BottomActivity).supportActionBar?.title = args.succursale!!.ville
 
-        //afficher les informations de la succurale sélectionnée
+        // EL : afficher les informations de la succurale sélectionnée
         txvVilleSuccursale.text = args.succursale!!.ville
         txvAdresseSuccursale.text = args.succursale!!.adresse
         txvInformationSuccursale.text = args.succursale!!.information
@@ -36,7 +45,9 @@ class detailsSuccursaleFragment : Fragment() {
         txvTelecopieurSuccursale.text = args.succursale!!.telecopieur
         txvAppelatif.text = args.succursale!!.appelatif
         txvCodePostalSuccursale.text = args.succursale!!.codePostal
-        txvVilleProvinceSuccursale.text = args.succursale!!.ville + " (" + args.succursale!!.province + ")"
+        txvVilleProvinceSuccursale.text = "${args.succursale!!.ville} (${args.succursale!!.province})"
+
+        setHasOptionsMenu(true)
     }
 
     companion object {
